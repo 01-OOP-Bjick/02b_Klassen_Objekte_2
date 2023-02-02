64b73a86848840b5fda61b60d3ca4cc357d8356c
bjick84 <bjick@web.de>
2023-02-02 09:10:07 +0100

    couter++

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,37 +5,50 @@ public class Cat {
   private String name;
   private String furColor;
   private int age; 
-  private boolean isfemale;
+
+  private boolean isFemale;
+  private int counter;
 
 
-public Cat(String name, String furColor, int age, boolean female) {
+public Cat(String name, String furColor, int age, boolean isFemale) {
     this.name = name;
     this.furColor = furColor;
     this.age = age;
-    this.isfemale = female;
+    this.isFemale = isFemale;
   }
 
 public String getStringAttributes(String op) {
-    switch (op) {
-      case "#name":
-        return name;
-      case "#color":
-        return furColor;
-      default:
-        return "ERROR!";
-    } 
-}
-
-public String getAge() {
-  if (isfemale) {
-    return checkEscalationLevel();  
-  } 
-  else {
-    return String.valueOf(age);
+  switch (op) {
+    case "#name":
+      return name;
+    case "#color":
+      return furColor;
+    default:
+      return "ERROR!";
   }
 }
 
+public String getAge() {
+  if (isFemale) {
+    return checkEscalationLevel();
+  } else {
+    return String.valueOf(age);
+  }
+}
+  
 private String checkEscalationLevel() {
-  return "This is an inappropriate question!";
+ 
+  counter++;  // +1
+
+  switch (counter) {
+    case 1:
+      return "This is an inappropriate question!";
+    case 2:
+      return "I've told you once!"; 
+    case 3:
+      return "Talk to the hand!";         
+    default:
+      return "1#!?&&%"; 
+  }
 }
 }
